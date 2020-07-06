91d47287f9f17d0fc4087dde30946b767436a1b8
Zhu Zidian <zhuzidian@gmail.com>
2020-07-06 18:16:04 +0900

    sample

--- camera.js
+++ camera.js
@@ -1,24 +1,59 @@
 window.onload = () => {
-  const errors = document.getElementById('errors')
+  // console.log('location.hostname', location.hostname)
+  // if (location.hostname !== 'localhost') {
+  //   var log = document.getElementById('log');
+  //   console.log = function (message) {
+  //     if (typeof message === 'object') {
+  //       log.innerHTML += JSON.stringify(message) + '<br>';
+  //     } else {
+  //       log.innerHTML += message + '<br>';
+  //     }
+  //   }
+  // }
+
   if (!navigator.mediaDevices) {
-    // console.log("navigator.mediaDevices not supported.")
-    errors.innerHTML += `<p>navigator.mediaDevices not supported</p>`
+    console.log("navigator.mediaDevices not supported.")
     return
   }
   if (!window.MediaRecorder) {
-    errors.innerHTML += `<p>MediaRecorder not supported</p>`
+    console.log("MediaRecorder not supported.")
     return
   }
 
-  const camera_video = document.getElementById("camera_video")
+  const sample_video = document.getElementById('sample_video')
+  const sample_play = document.getElementById('sample_play')
+  const sample_pause = document.getElementById('sample_pause')
+  // sample_video.onloadedmetadata = (event) => {
+  //   console.log('sample_video loadedmetadata', event);
+  // }
+  sample_play.onclick = (e) => {
+    sample_video.play()
+  }
+  sample_pause.onclick = (e) => {
+    sample_video.pause()
+  }
+
+
+  const camera_video = document.getElementById('camera_video')
   const playback_video = document.getElementById('playback_video')
   const downloadlink = document.getElementById('downloadlink')
 
-  const record_start = document.getElementById("rec_start")
-  const record_stop = document.getElementById("rec_stop")
+  const record_start = document.getElementById('rec_start')
+  const record_stop = document.getElementById('rec_stop')
+  const playback_play = document.getElementById('playback_play')
+  const playback_pause = document.getElementById('playback_pause')
+  playback_play.onclick = () => {
+    playback_video.play()
+  }
+  playback_pause.onclick = () => {
+    playback_pause.pause()
+  }
+
+  let mixed_video_stream = new MediaStream()
 
   // カメラ設定
   var constraints = {
+    // audio: { echoCancellation: true },
     audio: false,
     video: {
       aspectRatio: 300 / 200,
@@ -27,43 +62,53 @@ window.onload = () => {
     },
   }
   // カメラの内容を<vedio>に映る
-  navigator.mediaDevices
-    .getUserMedia(constraints)
+  navigator.mediaDevices.getUserMedia(constraints)
     .then((mediastream) => {
       camera_video.srcObject = mediastream
       camera_video.onloadedmetadata = (e) => {
         camera_video.play()
       }
 
-      const mediaRecorder = new MediaRecorder(mediastream)
+      // const mediaRecorder = new MediaRecorder(mediastream)
+      let mediaRecorder
 
       record_start.onclick = () => {
+        let track1 = mediastream.getVideoTracks()[0]
+        console.log('track1', track1)
+      
+        let track2 = sample_video.captureStream().getVideoTracks()[0]
+        console.log('track2', track2)
+      
+        mixed_video_stream.addTrack(track1)
+        mixed_video_stream.addTrack(track2)
+        console.log('mixed_video_stream', mixed_video_stream.getVideoTracks());
+        
+        mediaRecorder = new MediaRecorder(mixed_video_stream)
+
         mediaRecorder.start()
         console.log("recorder start", mediaRecorder.state)
-        errors.innerHTML += `<p>recorder start</p>`
+
+        mediaRecorder.ondataavailable = (e) => {
+          console.log(e)
+            
+          // const blob = new Blob([e.data], { type: e.data.type })
+          const blob = new Blob([e.data], { type: e.data.type })
+          const bloburl = URL.createObjectURL(blob)
+          playback_video.src = bloburl
+  
+          downloadlink.download = 'mixed.webm'
+          downloadlink.href = bloburl
+        }
       }
 
       record_stop.onclick = () => {
         mediaRecorder.stop()
-        console.log("recorder stop", mediaRecorder.state)
-        errors.innerHTML += `<p>recorder stop</p>`
-      }
-
-      mediaRecorder.ondataavailable = (e) => {
-        console.log('e.data.type', e.data.type)
-        errors.innerHTML += `<p>${e.data.type}</p>`
-          
-        const blob = new Blob([e.data], { type: e.data.type })
-        const bloburl = URL.createObjectURL(blob)
-        playback_video.src = bloburl
-
-        downloadlink.download = 'record.mp4'
-        downloadlink.href = bloburl
+        sample_video.pause()
+        console.log("recorder stop", mediaRecorder)
       }
     })
     .catch(function (err) {
       console.log(err.name + ": " + err.message)
-      errors.innerHTML += `<p>${err.name}: ${err.message}</p>`
     })
 
   // const photo_canvas = document.getElementById("photo")
