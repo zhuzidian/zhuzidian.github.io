38ada6cc1f48ac7f1b30d07647e4ecf3cb753b9e
Zhu Zidian <zhuzidian@gmail.com>
2020-07-07 16:09:16 +0900

    update

--- camera.js
+++ camera.js
@@ -21,113 +21,6 @@ window.onload = () => {
   }
 
   main()
-
-  // const sample_video = document.getElementById('sample_video')
-  // const sample_play = document.getElementById('sample_play')
-  // const sample_pause = document.getElementById('sample_pause')
-  // sample_play.onclick = (e) => {
-  //   sample_video.muted = false
-  //   sample_video.play()
-  // }
-  // sample_pause.onclick = (e) => {
-  //   sample_video.pause()
-  // }
-
-
-  
-  
-  // const ctx = playback_canvas.getContext('2d')
-  // const downloadlink = document.getElementById('downloadlink')
-
-  // const record_start = document.getElementById('rec_start')
-  // const record_stop = document.getElementById('rec_stop')
-  // const playback_play = document.getElementById('playback_play')
-  // const playback_pause = document.getElementById('playback_pause')
-  // playback_play.onclick = () => {
-  //   playback_video.play()
-  // }
-  // playback_pause.onclick = () => {
-  //   playback_video.pause()
-  // }
-
-  // let mixed_video_stream = new MediaStream()
-
-  // // カメラ設定
-  // var constraints = {
-  //   // audio: { echoCancellation: true },
-  //   audio: false,
-  //   video: {
-  //     width: { ideal: 300 },
-  //     height: { ideal: 200 },
-  //     // aspectRatio: 300 / 200,
-  //     facingMode: "user"   // フロントカメラ
-  //     // facingMode: { exact: "environment" }  // バックカメラ
-  //   },
-  // }
-  // // カメラの内容を<vedio>に映る
-  // navigator.mediaDevices.getUserMedia(constraints)
-  //   .then((mediastream) => {
-  //     console.log(mediastream.getVideoTracks()[0]);
-      
-  //     camera_video.srcObject = mediastream
-  //     camera_video.onloadedmetadata = (e) => {
-  //       camera_video.play()
-  //     }
-
-  //     (function video2Canvas () {
-	// 			ctx.drawImage(camera_video, 0, 0)
-	// 			// ctx.drawImage(sample_video, 0, 0, 640, 480, 10, 10, 160, 120)
-	// 			requestAnimationFrame(video2Canvas)
-	// 		})();
-
-  //     // const mediaRecorder = new MediaRecorder(mediastream)
-  //     // let mediaRecorder
-
-  //     // record_start.onclick = () => {
-  //     //   let track1 = sample_video.getVideoTracks()[0]
-  //     //   console.log('track1', track1)
-      
-  //     //   let track2 = playback_canvas.captureStream().getVideoTracks()[0]
-  //     //   console.log('track2', track2)
-      
-  //     //   mixed_video_stream.addTrack(track1)
-  //     //   mixed_video_stream.addTrack(track2)
-  //     //   console.log('mixed_video_stream', mixed_video_stream.getVideoTracks());
-        
-  //     //   mediaRecorder = new MediaRecorder(mixed_video_stream)
-
-  //     //   mediaRecorder.start()
-  //     //   console.log("recorder start", mediaRecorder.state)
-
-  //     //   mediaRecorder.ondataavailable = (e) => {
-  //     //     console.log(e)
-            
-  //     //     // const blob = new Blob([e.data], { type: e.data.type })
-  //     //     const blob = new Blob([e.data], { type: e.data.type })
-  //     //     const bloburl = URL.createObjectURL(blob)
-  //     //     // playback_video.src = bloburl
-  
-  //     //     downloadlink.download = 'mixed.webm'
-  //     //     downloadlink.href = bloburl
-  //     //   }
-  //     // }
-
-  //     // record_stop.onclick = () => {
-  //     //   mediaRecorder.stop()
-  //     //   sample_video.pause()
-  //     //   console.log("recorder stop", mediaRecorder)
-  //     // }
-  //   })
-  //   .catch(function (err) {
-  //     console.log(err.name + ": " + err.message)
-  //   })
-
-  // const photo_canvas = document.getElementById("photo")
-  // const shutter = document.getElementById("shutter")
-  // shutter.onclick = (e) => {
-  //   const ctx = photo_canvas.getContext("2d")
-  //   ctx.drawImage(camera_video, 0, 0, photo_canvas.width, photo_canvas.height)
-  // }
 }
 
 function main() {
@@ -135,7 +28,7 @@ function main() {
   let sample_video = document.getElementById('sample_video')
 
   let mixed_canvas = document.getElementById('mixed_canvas')
-  let mixed_ctx = mixed_canvas.getContext('2d')
+  let mixed_context = mixed_canvas.getContext('2d')
   
   let mixed_video = document.getElementById('mixed_video')
   let download_link = document.getElementById('download_link')
@@ -149,9 +42,9 @@ function main() {
     // audio: { echoCancellation: true },
     audio: false,
     video: {
-      width: 300,
-      height: 200, 
-      // aspectRatio: 300 / 200,
+      width: 320,
+      height: 180, 
+      // aspectRatio: 320 / 180,
       facingMode: 'user' // フロントカメラ
     },
   }
@@ -159,43 +52,93 @@ function main() {
     .getUserMedia(constraints)
     .then((camera_stream) => {
       camera_video.srcObject = camera_stream
-      camera_video.onloadedmetadata = (e) => {
+      camera_video.onloadedmetadata = () => {
         camera_video.play()
       }
 
-      sample_video.src = "sample.mp4"
+      sample_video.src = "missile.mp4"
       sample_video.onloadedmetadata = () => {
         sample_video.play()
       }
 
-      (function video2Canvas () {
-				mixed_ctx.drawImage(camera_video, 0, 0)
-				mixed_ctx.drawImage(sample_video, 10, 10, 128, 72)
-				requestAnimationFrame(video2Canvas)
-      })();
-      
-      let mixed_video_stream = new MediaStream()
-      let media_recorder
+      video2Canvas()
 
-      record_start.onclick = () => {
-        mixed_video_stream.addTrack(mixed_canvas.captureStream().getTracks()[0])
+      // let mixed_video_stream = new MediaStream()
+      // let media_recorder
+
+      // record_start.onclick = () => {
+      //   mixed_video_stream.addTrack(mixed_canvas.captureStream().getTracks()[0])
         
-        media_recorder = new MediaRecorder(mixed_video_stream)
-        media_recorder.start();
-        media_recorder.ondataavailable = (e) => {
-					var blob_url = URL.createObjectURL(e.data)
-					mixed_video.src = blob_url
-					download_link.download = 'mixed.webm'
-					download_link.href = blob_url
-				}
-      }
+      //   media_recorder = new MediaRecorder(mixed_video_stream)
+      //   media_recorder.start();
+      //   media_recorder.ondataavailable = (e) => {
+			// 		var blob_url = URL.createObjectURL(e.data)
+			// 		mixed_video.src = blob_url
+			// 		download_link.download = 'mixed.webm'
+			// 		download_link.href = blob_url
+			// 	}
+      // }
 
-      record_stop.onclick = () => {
-				// video.pause();
-				media_recorder.stop()
-			}
+      // record_stop.onclick = () => {
+			// 	// video.pause();
+			// 	media_recorder.stop()
+			// }
     })
-    .catch(err => {
-      console.log(err.name + ": " + err.message)
-    })
+
+  const video2Canvas = () => {
+    // 320*180
+    mixed_context.drawImage(camera_video, 0, 0)
+    // 160*90
+    mixed_context.drawImage(sample_video, 0, 0, sample_video.width, sample_video.height)
+    chromaKey()
+    requestAnimationFrame(video2Canvas)
+  }
+
+  var chromaKey = function () {
+    var imageData = mixed_context.getImageData(0, 0, sample_video.width, sample_video.height)
+
+    // dataはUint8ClampedArray
+    // 長さはcanvasの width * height * 4(r,g,b,a)
+    // 先頭から、一番左上のピクセルのr,g,b,aの値が順に入っており、
+    // 右隣のピクセルのr,g,b,aの値が続く
+    // n から n+4 までが1つのピクセルの情報となる
+    for (var i = 0; i < imageData.data.length; i += 4) {
+      const r = imageData.data[i];
+      const g = imageData.data[i + 1];
+      const b = imageData.data[i + 2];      
+      // 閾値
+      const condition = r < 80 && g > 200 && b < 80;
+      if (condition) {
+        imageData.data[i + 3] = 0;
+      }
+      // var target_rgb = {
+      //   r: imageData.data[i],
+      //   g: imageData.data[i + 1],
+      //   b: imageData.data[i + 2]
+      // };
+
+      // // chromaKeyColorと現在のピクセルの三次元空間上の距離を閾値と比較する
+      // // 閾値より小さい（色が近い）場合、そのピクセルを消す
+      // const distance = getColorDistance(chroma_key_rgb, target_rgb)
+      // if (distance < colorDistance) {
+      //   // alpha値を0にすることで見えなくする
+      //   imageData.data[i + 3] = 0;
+      // }
+    }
+    
+    mixed_context.putImageData(imageData, 0, 0);
+  };
+  
+  var getColorDistance = function (rgb1, rgb2) {
+    // 三次元空間の距離が返る
+    return Math.sqrt(
+        Math.pow((rgb1.r - rgb2.r), 2) +
+        Math.pow((rgb1.g - rgb2.g), 2) +
+        Math.pow((rgb1.b - rgb2.b), 2)
+    );
+  };
 }
+
+// 消す色と閾値
+var chroma_key_rgb = { r: 0, g: 255, b: 0 }
+var colorDistance = 100
